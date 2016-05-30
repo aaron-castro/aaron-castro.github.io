88628b531c171e261b570248d12bbc8f343cff57
aaron <liyng23@gmail.com>
2016-05-29 23:50:37 -0500

    DESCRIPCIÓN

---
+++ structure/content/content-empleos.php
@@ -0,0 +1,214 @@
+<div class="chamba">
+	<section class="container">
+				<div class="col-lg-12 ">
+					<h1 class="company text-center">	
+						Consorcio Laboro
+						
+					</h1>
+					<div class="col-lg-7">
+						<p class="tiempo text-left"> <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> &nbsp;<ins >  5 de Noviembre del 2015 - 20  de Febrero del 2016 </ins></p>
+					</div>
+					<div class="col-lg-5">
+						<p class="tiempo text-right"> <span class=" glyphicon glyphicon-folder-close" aria-hidden="true"></span> &nbsp;<ins >Desarrollador Web</ins></p>
+					</div>
+					
+						
+
+						<p class="text-justify descripcion">  Funciones desempeñadas:
+						<br> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Modificaciones a los módulos de los sistemas de la compañía. También se desarrollaron soluciones para las nuevas necesidades de la empresa ..<br>
+						<br> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Sistema de sorteos: Donde se regalan beneficios para el personal que administra la empresa, (obteniendo la información de los registros de los usuarios de la base de datos) notificando a los ganadores vía correo y proporcionando un enlace para aceptar los términos y condiciones del sorteo (el cual se guarda en la base de datos para futuras certificaciones de ISO) aceptando los términos y condiciones se les enviará por correo un enlace para canjear su beneficio obtenido.   <br>
+							<br> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Campañas de mailing para: Servicios para clientes, prospectos de clientes y ex clientes con el objetivo de mantener, atraer, y retomar a los clientes.</p><br>
+
+				
+				</div>
+
+
+	</section>
+</div>
+<div class="">
+	<section class="container">
+				<div class="col-lg-12 ">
+					<h1 class="company text-center">	
+						Mampower
+						
+					</h1>
+					<div class="col-lg-7">
+						<p class="tiempo text-left"> <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> &nbsp;<ins >  1 de Octubre del 2015 - 3 de Noviembre del 2015</ins></p>
+					</div>
+					<div class="col-lg-5">
+						<p class="tiempo text-right"> <span class=" glyphicon glyphicon-folder-close" aria-hidden="true"></span> &nbsp;<ins >Soporte Técnico- Formula 1</ins></p>
+					</div>
+					
+						
+
+						<p class="text-justify descripcion">  Funciones desempeñadas:
+						<br> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Configuraciones de sistemas de puntos de ventas a servidores.<br>
+						<br> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Consultas de ventas en el servidor . <br>
+							<br> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Instalación de puntos de ventas a servidores de la red.</p> <br>
+
+				
+				</div>
+
+
+	</section>
+</div>
+
+
+
+
+
+<div class="chamba">
+	<section class="container chamba">
+		<div class="col-lg-12 chamba">
+					<h1 class="company text-center">	
+						Parroquia Nuestra Señora de la Anunciación
+						
+					</h1>
+					<div class="col-lg-8">
+						<p class="tiempo text-left"> <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> &nbsp;<ins >  1 de Septiembre  del 2015  - 25 de Septiembre del 2015</ins></p>
+					</div>
+					<div class="col-lg-4">
+						<p class="tiempo text-right"> <span class=" glyphicon glyphicon-folder-close" aria-hidden="true"></span> &nbsp;<ins >Desarrollo Web Freelance</ins></p>
+					</div>
+						</div>
+		<div class="col-lg-5 chamba">
+				<p class="text-justify descripcion">
+						Las carecteristicas principales de la  página  webweb:
+							<ul class="text-justify media-list">
+							<li>
+								<p class="description"> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp; Incluye  widgets  de google, facebook y twitter.</p>
+							</li>
+							<li>
+								<p class="description"><span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp; Contiene su propio gestor de contendio , administrado por la parroquia.</p>
+							</li>
+							<li>
+									<p class="description"><span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp; Permisos con la Arquidiocesis  de Tlalnepantla</p><br>
+							</li>
+							<li>
+									<p class="description"><span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp; Diseño responsivo.</p><br>
+							</li>
+
+								
+							</ul>
+				</p>
+			
+		</div>
+
+		<figure	class="col-lg-7 ">
+		<a href="http://parroquiaanunciacionechegaray.org.mx/" data-toggle="tooltip" class="imagen "title="Te invito a que conoscas la   página  web ">
+			<img src="img/empleos/pageechegaray.png" class=" img-responsive"alt="Parroquia de Echegaray" >
+			
+			<p class="text-center manita"><span class=" text-left glyphicon glyphicon-hand-up" aria-hidden="true"></span><p>
+				</a>
+		</figure	>
+		
+
+	</section>
+	
+</div>
+
+
+<div class="">
+	<section class="container">
+		<div class="col-lg-12 ">
+					<h1 class="company text-center">	
+						Parroquia Nuestra Señora de Lourdes
+						
+					</h1>
+					<div class="col-lg-7">
+						<p class="tiempo text-left"> <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> &nbsp;<ins >  1 de Junio  del 2015  - 20 de Junio del 2015</ins></p>
+					</div>
+					<div class="col-lg-5">
+						<p class="tiempo text-right"> <span class=" glyphicon glyphicon-folder-close" aria-hidden="true"></span> &nbsp;<ins >Desarrollo Web Freelance</ins></p>
+					</div>
+						</div>
+						<figure	class="col-lg-7 ">
+						<a href="http://parroquianuestrasenoradelourdes.org/" class="imagen "data-toggle="tooltip" title="Te invito a que conoscas la   página  web">
+							<img src="img/empleos/pagelourdes.png" class="img-responsive" alt="Parroquia de nuestra señora de lourdes">
+									<p class="text-center manita-left"><span class=" text-left glyphicon glyphicon-hand-up" aria-hidden="true"></span><p>
+							</a>
+						</figure	>
+						
+		
+		<div class="col-lg-5 ">
+				<p class="text-justify descripcion">
+						Las carecteristicas principales de la  página  webweb:
+							<ul class="text-justify media-list">
+							<li>
+								<p class="description"> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Incluye  widgets  de google</p>
+							</li>
+							<li>
+								<p class="description"> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Diseño de imagenes</p>
+							</li>
+							<li>
+									<p class="description"> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Permisos con la Arquidiocesis  de Tlalnepantla</p>
+							</li>
+
+							<li>
+									<p class="description"> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Diseño responsivo.</p><br>
+							</li>
+
+
+								
+							</ul>
+				</p>
+			
+		</div>
+
+		
+
+	</section>
+	
+</div>
+
+
+
+
+<div class="chamba">
+	<section class="container chamba">
+
+		<div class="col-lg-12 chamba">
+					<h1 class="company text-center">	
+						Ld Suministros Industriales
+						
+					</h1>
+					<div class="col-lg-7">
+						<p class="tiempo text-left"> <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> &nbsp;<ins >  1 de Abril del 2015  -  1 de Mayo  del 2015</ins></p>
+					</div>
+					<div class="col-lg-5">
+						<p class="tiempo text-right"> <span class=" glyphicon glyphicon-folder-close" aria-hidden="true"></span> &nbsp;<ins >Desarrollo Web Freelance</ins></p>
+					</div>
+						</div>
+		<div class="col-lg-5 chamba">
+				<p class="text-justify descripcion">
+						Las carecteristicas principales de la  página  webweb:
+							<ul class="text-justify media-list">
+							<li class="media-left">
+								<p class="description"> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Muestra El Catálogo De la Empresa.</p>
+							</li>
+							<li>
+								<p class="description"> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Se administro el contenio de las publicaciones en Facebook.</p>
+							</li>
+							
+							<li>
+									<p class="description"> <span class="glyphicon glyphicon-ok " aria-hidden="true"></span> &nbsp;Diseño responsivo.</p><br>
+							</li>
+
+								
+							</ul>
+				</p>
+			
+		</div>
+
+		<figure	class="col-lg-7 ">
+		<a href="http://ld.org.mx/" data-toggle="tooltip" class="imagen" title="Te invito a que conoscas la   página  web">
+			<img src="img/empleos/pageld.png" class="img-responsive" alt="Empresa ld Suministros Industriales">
+					<p class="text-center manita"><span class=" text-left glyphicon glyphicon-hand-up" aria-hidden="true"></span><p>
+					</a>
+
+		</figure	>
+		
+
+	</section>
+	
+</div>
---
+++ structure/head.php
@@ -0,0 +1,20 @@
+		<title> cv aaron ......> </title>
+		<link rel="icon" type="image/png" href="img/faviicon.png" />
+	<link href='https://fonts.googleapis.com/css?family=Lato:400,300,900,700' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=PT+Sans:400italic' rel='stylesheet' type='text/css'>
+	
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</script>
+

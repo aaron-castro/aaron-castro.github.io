f96a015ef7b07083309b2cd63356557cf160069f
aaron-marley <liyng23@gmail.com>
2016-07-02 14:29:28 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,133 @@
+ var app= angular.module('empleos',[]);
+
+ app.controller('EmpresaController',function(){
+
+ 	this.labore= company;
+ });
+app.controller('TabController', function(){
+    this.tab = 1;
+
+    this.setTab = function(newValue){
+      this.tab = newValue;
+    };
+
+    this.isSet = function(tabName){
+      return this.tab === tabName;
+    };
+  });
+ var company = [
+ {
+ 	nameclase:"",
+ 	nav:'list2',
+ 	nombre:'Parroquia Anunciacion de Echegaray.',
+ 	url:'http://parroquiaanunciacionechegaray.org.mx',
+ 	enlace:'Sitio Web Desarollado',
+ 	classenlace:'enlacebtn',
+ 	giro:'Iglesia.',
+ 	logo:'img/empleos/frenlace.jpg',
+ 	puesto:'Desarrollador Web Freenlace.',
+ 	lugar:'Nuestra Ubicación Hacienda San Nicolas Tolentino No.115 Col. Bosques de Echegaray.',
+ 	tel:'53-92-55-08.',
+ 	tiempo:'Marzo del 2016 - Junio del 2016.',
+ 	actividaduno:'Desarrollo de Envio de correo,login, Generador y Gestor De Publicaciones (altas,bajas,cambio de las publicaciones realizadas) .',
+ 	actividaddos:'Manejo  de Redes Sociales (Facebook,Twitter)',
+ 	actividadtres:'Diseño de  Pagina Web ',
+ 	tecnologias:' PHP 5, PHP MAILER, MYSQL, JAVASCRIPT, JQUERY,HTML 5, CSS3, Bootstrap , Diseño Responsivo. ',
+
+ 	
+
+
+ },
+ {
+ 	nameclase:"chamba",
+ 	nav:'list2',
+ 	nombre:'Consorcio Laboro.',
+ 	url:'#',
+ 	enlace:'',
+ 	 	classenlace:'',
+ 	giro:'Outsoursing.',
+ 	logo:'img/empleos/outsourcing.jpg',
+ 	puesto:'Desarrollador Web.',
+ 	lugar:'Satelite, Estado de México.',
+ 	tel:'53-93-07-00.',
+ 	tiempo:'Noviembre del 2015 - Marzo del 2016.',
+ 	actividaduno:'Modificasciones a módulos de los sistemas web de la compañia desarrollando soluciones para las nuevas necesidades de la empresa.',
+ 	actividaddos:'Envió masivo de mail utilizando  para diferentes servicios de la empresa construcción de plantillas personalizadas para cada campaña.',
+ 	actividadtres:'Sistemas de sorteo de beneficios para el personal que administra la empresa notificando por correo, llevando un processo de aceptación de terminos y condiciones para canjear su beneficio, llevando un registro en la base de datos.',
+ 	tecnologias:'PHP 4, PHP 5, MYSQL, JAVASCRIPT,JQUERY, HTML 5, CSS3, Bootstrap , Diseño Responsivo.  ',
+
+ 	
+
+
+ },
+
+
+ {
+ 	nameclase:" ",
+ 	nav:"list2",
+ 	nombre:'Mampower.',
+ 	url:'#',
+ 	enlace:'',
+ 	classenlace:'',
+ 	giro:'Outsoursing.',
+ 	logo:'img/empleos/outsourcing.jpg',
+ 	puesto:'Soporte Técnico Analista.',
+ 	lugar:' Calzada San Simon 208, Cuauhtémoc, Col. San Simón Tolnáhuac, 06920 Ciudad de México.',
+ 	tel:'01-55-4147-5000.',
+ 	tiempo:'Octubre del 2015 - Noviembre del 2015.',
+ 	
+ 	actividaduno:'Configuraciones de sistemas de puntos de ventas a servidores.',
+ 	actividaddos:'Consultas de ventas en el servidor. ',
+ 	actividadtres:'Instalación de puntos de ventas a servidores de la red. ',
+ 	
+ 	tecnologias:'Sql Server 2010. ',
+ 	
+ },
+ {
+ 	nameclase:"chamba ",
+ 	nav:"list2",
+ 	nombre:'Sertec.',
+ 	url:'#',
+ 	enlace:'',
+ 	classenlace:'',
+ 	giro:'Gestion de Cobranza.',
+ 	logo:'img/empleos/sertec.jpg',
+ 	puesto:'Programador  Analista.',
+ 	lugar:'SERTEC, Avenida Benjamín Franklin No. 98, Planta Baja, Cuauhtémoc, Hipódromo, 11800 Ciudad de México.',
+ 	tel:' 01-55-52-78-43-27.',
+ 	tiempo:'Marzo del 2015 - Septiembre del 2015.',
+ 	
+ 	actividaduno:'Modificación de módulos de los sistemas Web de la compañia.',
+ 	actividaddos:'Amplio Manejo de base de datos para generar reportes de acuerdo a las necesidades de cada departamento. ',
+ 	actividadtres:'Validaciones de campos. ',
+ 	tecnologias:'PHP 5,HTML 5,MYSQL, JQUERY . ',
+
+
+ 	
+ },
+
+{
+ 	nameclase:" ",
+ 	nav:"list2",
+ 	nombre:'Ld Suministros Industriales.',
+ 	url:'http://ld.org.mx/',
+ 	enlace:'Sitio Web Desarollado',
+ 	classenlace:'enlacebtn',
+ 	giro:'Comercializadora de productos industriales.',
+ 	logo:'img/empleos/frenlace.jpg',
+ 	puesto:'Desarrollador Web Freenlace.',
+ 	lugar:'San Juan Ixtacala,Tlalnepantla de Baz Estado de México.',
+ 	tel:' 58-94-16-06.',
+ 	tiempo:'Abril del 2015 - Mayo del 2015.',
+ 	
+ 	actividaduno:'Desarrollo y Diseño de la página web.',
+ 	actividaddos:'Diseño responsivo. ',
+ 	actividadtres:'Manejo de Redes Sociales . ',
+ tecnologias:' PHP 5, PHP MAILER, JAVASCRIPT, JQUERY,HTML 5, CSS3, Bootstrap , Diseño Responsivo. ',
+
+
+ 	
+ },
+
+
+ ];

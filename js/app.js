1038adac1c922aadb6056ea2020c9ca342089f3a
aaron-marley <liyng23@gmail.com>
2016-07-12 18:41:00 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ app.controller('TabController', function(){
  {
  	nameclase:"",
  	nav:'list2',
- 	nombre:'Freenlace-Parroquia Anunciacion de Echegaray.',
+ 	nombre:'Frelance.',
  	url:'http://parroquiaanunciacionechegaray.org.mx',
  	enlace:'Sitio Web Desarollado',
  	classenlace:'enlacebtn',
@@ -109,7 +109,7 @@ app.controller('TabController', function(){
 {
  	nameclase:" ",
  	nav:"list2",
- 	nombre:'Freenlace-Ld Suministros Industriales.',
+ 	nombre:'Frelance.',
  	url:'http://ld.org.mx/',
  	enlace:'Sitio Web Desarollado',
  	classenlace:'enlacebtn',
